295bd4e5b9d4fb66e5520ccabf1738fa3914424c
ftambler <federicotambler@gmail.com>
2025-04-20 16:33:09 -0300

    small fix

--- __init__.py
+++ __init__.py
@@ -1,42 +1,42 @@
 import cv2
+import cv2.data
 import numpy as np
 from PIL import Image
 from imgbeddings import imgbeddings
 import base64
 import io
+import json
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-haar_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
+haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +  'haarcascade_frontalface_default.xml')
 
 @app.route('/', methods=['POST'])
 def enpoin():
+    data = json.loads(request.data)
+    base64String = data['base64String']
 
-    data = request.files['file']
-
-    # image_data = base64.b64decode(data)
-    image = Image.open(io.BytesIO(bytearray(data.read())))
-
-    current_frame = np.array(image)
+    image = Image.open(io.BytesIO(base64.b64decode(base64String)))
+    current_frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
     current_faces = haar_cascade.detectMultiScale(current_frame, scaleFactor=1.05, minNeighbors=3, minSize=(300, 300))
-
     if len(current_faces) < 1:
-        exit()
-    else:
-        x, y, w, h = current_faces[0]
+        return jsonify({'error': 'No face detected'}), 400
 
+    x, y, w, h = current_faces[0]
     image = current_frame[y:y+h, x:x+w]
-
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('resources/gray-face.jpg', gray_image)
-
+    
     ibed = imgbeddings()
-    embedding = ibed.to_embeddings('resources/gray-face.jpg')[0]
+    _, buffer = cv2.imencode('.jpg', gray_image)
+    embedding = ibed.to_embeddings(Image.open(io.BytesIO(buffer)))[0] 
 
-    return jsonify({'vector': embedding.tolist(), 'image_bytes': gray_image.tolist()})
+    return jsonify({
+        'vector': embedding.tolist(), 
+        'base64String': base64.b64encode(buffer).decode('utf-8')
+    })
 
 if __name__ == '__main__':
     app.run()
